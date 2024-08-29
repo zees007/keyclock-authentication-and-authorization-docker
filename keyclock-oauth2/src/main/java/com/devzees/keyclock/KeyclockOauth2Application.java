15a9dc45f68a3596537fc38c616f437c70fb6aa8
zees007 <zees007@rediffmail.com>
2024-08-30 01:08:05 +0530

    initial project set up

---
+++ keyclock-oauth2/src/main/java/com/devzees/keyclock/KeyclockOauth2Application.java
@@ -0,0 +1,13 @@
+package com.devzees.keyclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyclockOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyclockOauth2Application.class, args);
+	}
+
+}
---
+++ keyclock-oauth2/src/test/java/com/devzees/keyclock/KeyclockOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devzees.keyclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyclockOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
